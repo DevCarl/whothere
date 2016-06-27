650c24a79b43dce766b92a3ae9612a62d58b6480
cokelly <okellyconor@gmail.com>
2016-06-27 10:40:31 +0100

    Put code into functions. Start of unit testing for functions

--- data_cleaning/timetable_cleaner.py
+++ data_cleaning/timetable_cleaner.py
@@ -6,15 +6,36 @@
 import unittest, csv, json, re
 
 
+def convert_csv_to_array(directory):
 
-with open("1.test_data/B0.02 B0.03 B0.04 Timetable.csv", "r") as csv_file:
-    file_ouput = csv.reader(csv_file)
+    try:
+        with open(directory, "r") as csv_file:
+            file_ouput = csv.reader(csv_file)
+
+            # Turn csv object into 3D array
+            csv_rows = []
+            for current_row in file_ouput:
+                csv_rows.append(current_row)
+
+            print(csv_rows[0])
+
+
+    except FileNotFoundError:
+        csv_rows = []
+        print("File not found")
+
+    return csv_rows
+
+def sort_csv_output_return_json(csv_array):
 
     # Sort through the rows of the file
     # First row contains meta information about dates
 
     ### First build a picture of the data contained. i.e => number of week present / if each week has 5 days / dates
 
-    print(file_ouput)
-    for row in file_ouput:
-        print(row)
+    return 0
+
+# def test_that_multidimensional_array_returned:
+
+if __name__ == '__main__':
+    convert_csv_to_array("1.test_data/B0.02 B0.03 B0.04 Timetable.csv")
