00c8f1e4f6d59673e14a998cd851f3860b5e0362
cokelly <okellyconor@gmail.com>
2016-07-01 15:34:04 +0100

    Returning pandas data frame using devins code. Start formating into dict array

--- data_cleaning/occupancy_report_phraser.py
+++ data_cleaning/occupancy_report_phraser.py
@@ -9,14 +9,76 @@ import nose2
 import openpyxl
 import csv
 
+import pandas as pd
+import dateutil.parser
+
 
 def phrase_occupancy_excel_file(target_directory):
 
-    excel_object = convert_excel_to_csv(target_directory)
-    occupancy_data_array = phrase_data_from_csi_sheet_in_workbook(excel_object)
+    pandas_data_frame = return_data_frame_from_excel_using_pandas(target_directory)
+    results_array = convert_data_frame_into_dict_array(pandas_data_frame)
+    # print(pandas_data_frame)
 
 
-def convert_excel_to_csv(directory):
+def return_data_frame_from_excel_using_pandas(directory):
+
+    df = pd.read_excel(open(directory, "rb"), sheetname = "CSI")
+    df = df.loc[df.count(axis=1) > 1]
+
+    first = True
+    df["Date"] = 0
+    date = 0
+    CSIList = ["CSI Classroom FREQUENCY OF USE", "CSI Classroom OCCUPANCY"]
+    CSICurrent = None
+    CSIStart, CSIEnd = 0, 0
+    for i in df.index:
+        try:
+            date = dateutil.parser.parse(df[0][i])
+        except:
+            pass
+        if df[0][i] == "CSI Classroom OCCUPANCY":
+            CSICurrent = df[0][i]
+            CSIStart = i
+        elif df[0][i] == "CSI Classroom UTILISATION":
+            dfOccupancy = df[(df.index >= CSIStart) & (df.index < i)]
+            dfOccupancy["Date"] = date
+            if first:
+                dfFull = dfOccupancy
+                first = False
+            else:
+                dfFull = pd.concat([dfFull, dfOccupancy])
+
+    building_list = ["CSI"]
+    check_columns = []
+    occupancy = {}
+
+    for column in dfFull:
+        if "CSI" in dfFull[column].values:
+            check_columns.append(column)
+
+    for i in dfFull.index:
+        if dfFull[check_columns[0]][i] in building_list:
+            for column in range(0, len(check_columns)):
+                column2 = dfFull[check_columns[column]][i] + "-" + dfFull[check_columns[column]][i+1]
+                dfFull = dfFull.rename(columns={check_columns[column]: column2})
+                check_columns[column] = column2
+                occupancy[column2] = dfFull[check_columns[column]][i+2]
+            break
+
+    dfFull = dfFull.rename(columns={0: "Time"})
+    timeslots = ["9.00-10.00", "10.00-11.00", "11.00-12.00", "12.00-13.00", "13.00-14.00", "14.00-15.00", "15.00-16.00", "16.00-17.00"]
+    dfFull = dfFull[dfFull['Time'].isin(timeslots)]
+    dfFull = dfFull.dropna(axis=1,how='all')
+
+    return dfFull
+
+
+def convert_data_frame_into_dict_array(pandas_data_frame):
+
+    print(pandas_data_frame[0:5])
+
+
+def load_excel_file(directory):
 
     try:
         # Load workbook with all sheets
