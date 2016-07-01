f22f8bbbfec5c7e9d4c87144d41de7c79f7fc46e
cokelly <okellyconor@gmail.com>
2016-07-01 11:45:59 +0100

    Timetable cleaner finished. Started unittesting for section

--- data_cleaning/timetable_cleaner.py
+++ data_cleaning/timetable_cleaner.py
@@ -1,40 +1,49 @@
 # Author Conor O'Kelly
 
-# Aim of this script will be to open the csv file and read in the csv data.
-##This data is then cleaned and returned as a json object
+# Aim of this script will be to take in the excel file data for the timetable. The data is first cleaned then
+# placed into dicts that are returned to be handling by the main controller
 
-import unittest, csv, openpyxl
+import unittest
+import openpyxl
+import nose2
 
 
-def main(directory):
+def phrase_excel_sheet_into_array_of_dicts(target_directory):
 
     # Function returns tuple of 0 is workbook object / 1 sheet objects
-    sheet_object = load_workbook_return_sheets_object(directory)
+    sheet_object = load_workbook_return_sheets_object(target_directory)
     unmerge_excel_cells_and_perserve_data(sheet_object[1])
 
     sheet_object[0].save("1.test_data/new.xlsx")
     # Convert sheets into dict of all informaiton
-    convert_sheets_into_dict(sheet_object[1])
+    timetable_info = convert_sheets_into_dict(sheet_object[1])
+
+    return timetable_info
 
 
 def load_workbook_return_sheets_object(directory):
 
-    # Load workbook with all sheets
-    work_book = openpyxl.load_workbook(directory)
+    try:
+        # Load workbook with all sheets
+        work_book = openpyxl.load_workbook(directory)
 
-    # Load a all sheets from workbook
-    sheet_names = work_book.get_sheet_names()
+        # Load a all sheets from workbook
+        sheet_names = work_book.get_sheet_names()
 
-    # Remove last sheet name if name == All
-    if sheet_names[-1] == "All" or sheet_names[-1] == "all":
-        sheet_names.pop(-1)
+        # Remove last sheet name if name == All
+        if sheet_names[-1] == "All" or sheet_names[-1] == "all":
+            sheet_names.pop(-1)
 
-    # Load sheet object into array
-    sheet_objects = []
-    for sh_name in sheet_names:
-        sheet_objects.append(work_book.get_sheet_by_name(sh_name))
+        # Load sheet object into array
+        sheet_objects = []
+        for sh_name in sheet_names:
+            sheet_objects.append(work_book.get_sheet_by_name(sh_name))
 
-    return (work_book,sheet_objects)
+        return (work_book,sheet_objects)
+
+    except FileNotFoundError:
+        print("The file at", directory,"could not be found.")
+        return (0,0)
 
 
 def unmerge_excel_cells_and_perserve_data(sheet_objects):
@@ -63,50 +72,83 @@ def unmerge_excel_cells_and_perserve_data(sheet_objects):
 
     return sheet_objects
 
+
 # Could come back and write this function to be able to handle a more dynamic approach #
 def convert_sheets_into_dict(sheet_objects):
 
-    file_dict = {}
+    timetable_information = []
 
     # Cycle through sheets and produce a dict for each one with timetable information
-    current_sheet = sheet_objects[0]
+    for current_sheet in sheet_objects:
+        current_sheet_array = []
 
-    current_sheet_array = []
+        # Determine number of weeks on page by checking the first row for data
+        if current_sheet["M2"].value is not None and current_sheet["B2"].value is not None:
+            no_weeks = 2
+        elif current_sheet["B2"].value is not None:
+            no_weeks = 1
+        else:
+            no_weeks = 0
 
-    # Determine number of weeks on page by checking the first row for data
-    if current_sheet["M2"].value is not None and current_sheet["B2"].value is not None:
-        no_weeks = 2
-    elif current_sheet["B2"].value is not None:
-        no_weeks = 1
-    else:
-        no_weeks = 0
-
-    # Get month from top second column in each week timetable
-    month = current_sheet["B1"].value.split(" ")[1]
-    # Cell color
-
-    # Cycle through columns and print first value
-    for col_letter in range(ord("B"),ord("J")+1,2):
-        day = current_sheet[chr(col_letter)+"2"].value
-        date = day.strip() + " " + month
-        for row_no in range(3,11+1):
-            time = current_sheet["A"+str(row_no)].value
-            module = current_sheet[chr(col_letter)+str(row_no)].value
-            no_expected_students = current_sheet[chr(col_letter)+str(row_no+1)].value
-            cell_color = current_sheet[chr(col_letter)+str(row_no)].fill.start_color.index
-            # Check if cell has color and if has value indicating class did not go ahead
-            if cell_color != "00000000" and module is not None:
-                class_appeared_to_go_ahead = False
+        for i in range(1,no_weeks+1):
+            # Set starting point for week
+            if i == 1:
+                start_column = "B"
+                finish_column = "J"
+            elif i == 2:
+                start_column = "N"
+                finish_column = "V"
             else:
-                class_appeared_to_go_ahead = True
-            time_slot ={"date": date, "time": time, "module": module, "no_expected_students": no_expected_students,
-                                       "class_appeared_to_go_ahead": class_appeared_to_go_ahead}
-            current_sheet_array.append(time_slot)
-    for i in current_sheet_array:
-        print(i)
+                start_column = "B"
+                finish_column = "B"
+
+            # Get month from top second column in each week timetable
+            month = current_sheet[start_column+"1"].value.split(" ")[1]
+            # Cell color
+
+            # Cycle through columns and print first value
+            for col_letter in range(ord(start_column),ord(finish_column)+1,2):
+                day = current_sheet[chr(col_letter)+"2"].value
+                date = day.strip() + " " + month
+                # print(date)
+                # Cycle through rows and extract the required data
+                for row_no in range(3,11+1):
+                    # print(chr(col_letter+1)+str(row_no))
+                    room = current_sheet[chr(ord(start_column)-1)+"1"].value
+                    time = current_sheet[chr(ord(start_column)-1)+str(row_no)].value
+                    module = current_sheet[chr(col_letter)+str(row_no)].value
+                    no_expected_students = current_sheet[chr(col_letter+1)+str(row_no)].value
+                    cell_color = current_sheet[chr(col_letter)+str(row_no)].fill.start_color.index
+                    # Check if cell has color and if has value indicating class did not go ahead
+                    if cell_color != "00000000" and module is not None:
+                        class_appeared_to_go_ahead = False
+                    else:
+                        class_appeared_to_go_ahead = True
+                    time_slot ={"room": room, "date": date, "time": time, "module": module,
+                                "no_expected_students": no_expected_students,
+                                "class_appeared_to_go_ahead": class_appeared_to_go_ahead}
+
+                    current_sheet_array.append(time_slot)
+
+            # Add array for single week to file return information
+            timetable_information.extend(current_sheet_array)
+
+    # print(timetable_information)
+    return timetable_information
 
 
+# Unittesting
 
+def test_workbook_loads_and_returns_workbook():
+    results = load_workbook_return_sheets_object("1.test_data/B0.02 B0.03 B0.04 Timetable.xlsx")
+    # Assert that first result of tuple is workbook objects
+    assert isinstance(results[0], openpyxl.Workbook)
+
+
+def test_unmerge_excel_cells_and_perserve_data():
+    # Test the first return sheet has no merged cells
+    assert True
 
 if __name__ == '__main__':
-    main("1.test_data/B0.02 B0.03 B0.04 Timetable.xlsx")
+    # x = phrase_excel_sheet_into_array_of_dicts("1.test_data/B0.02 B0.03 B0.04 Timetable.xlsx")
+    nose2.main()
