0c1fb2a1fe720df568c4a1c922c416c85e79c805
cokelly <okellyconor@gmail.com>
2016-07-01 11:52:07 +0100

    Added new phraser modules

---
+++ data_cleaning/occupancy_report_phraser.py
@@ -0,0 +1,5 @@
+# Author Devin Stacey and Conor O'Kelly
+
+
+if __name__ == '__main__':
+    print(1)
---
+++ data_cleaning/wifi_log_phraser.py
@@ -0,0 +1,4 @@
+# Author Devin Stacey and Conor O'Kelly
+
+if __name__ == '__main__':
+    print(1)
