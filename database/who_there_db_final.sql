a8526cae562121b23f2e9999b4a3e6acb34b5851
cokelly <okellyconor@gmail.com>
2016-07-05 10:48:33 +0100

    Final draft for database updated

--- database/who_there_db_final.sql
+++ database/who_there_db_final.sql
@@ -5,119 +5,122 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema who_there_db
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema who_there_db
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `who_there_db` DEFAULT CHARACTER SET utf8 ;
+USE `who_there_db` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`Room`
+-- Table `who_there_db`.`Room`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Room` (
+CREATE TABLE IF NOT EXISTS `who_there_db`.`Room` (
   `Room_id` VARCHAR(45) NOT NULL,
   `Room_no` VARCHAR(45) NULL,
   `Buildling` VARCHAR(45) NULL,
-  `floor_no` VARCHAR(45) NULL,
+  `Floor_no` VARCHAR(45) NULL,
   `Campus` VARCHAR(45) NULL,
   `Room_active` TINYINT(1) NULL,
-  `Capacity` VARCHAR(45) NULL,
+  `Capacity` INT NULL,
   `Plug_friendly` TINYINT(1) NULL,
-  PRIMARY KEY (`Room_id`))
+  PRIMARY KEY (`Room_id`),
+  UNIQUE INDEX `Room_id_UNIQUE` (`Room_id` ASC))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Module`
+-- Table `who_there_db`.`Module`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Module` (
-  `Module_code` INT NOT NULL,
-  `Facilty` VARCHAR(45) NULL,
-  `Course_level` VARCHAR(45) NULL,
-  `Undergrad` VARCHAR(45) NULL,
+CREATE TABLE IF NOT EXISTS `who_there_db`.`Module` (
+  `Module_code` VARCHAR(45) NOT NULL,
+  `Facilty` VARCHAR(90) NULL,
+  `Course_level` VARCHAR(90) NULL,
+  `Undergrad` TINYINT(1) NULL,
+  `Module_active` TINYINT(1) NULL DEFAULT 1,
   PRIMARY KEY (`Module_code`))
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`time_table`
+-- Table `who_there_db`.`time_table`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`time_table` (
-  `date` VARCHAR(45) NOT NULL,
-  `time_period` VARCHAR(45) NOT NULL,
+CREATE TABLE IF NOT EXISTS `who_there_db`.`time_table` (
+  `Date` DATE NOT NULL,
+  `Time_period` TIME NOT NULL,
   `Room_Room_id` VARCHAR(45) NOT NULL,
-  `Module_Module_code` INT NOT NULL,
+  `Module_Module_code` VARCHAR(45) NOT NULL,
   `No_expected_students` INT NULL,
   `Lecuter` TINYINT(1) NULL,
   `Tutorial` TINYINT(1) NULL,
-  `double_module` TINYINT(1) NULL,
-  PRIMARY KEY (`date`, `time_period`, `Room_Room_id`, `Module_Module_code`),
+  `Double_module` TINYINT(1) NULL DEFAULT 0 COMMENT '	',
+  PRIMARY KEY (`Date`, `Time_period`, `Room_Room_id`, `Module_Module_code`),
   INDEX `fk_time_table_Room1_idx` (`Room_Room_id` ASC),
   INDEX `fk_time_table_Module1_idx` (`Module_Module_code` ASC),
   CONSTRAINT `fk_time_table_Room1`
     FOREIGN KEY (`Room_Room_id`)
-    REFERENCES `mydb`.`Room` (`Room_id`)
+    REFERENCES `who_there_db`.`Room` (`Room_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_time_table_Module1`
     FOREIGN KEY (`Module_Module_code`)
-    REFERENCES `mydb`.`Module` (`Module_code`)
+    REFERENCES `who_there_db`.`Module` (`Module_code`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`wifi_log`
+-- Table `who_there_db`.`wifi_log`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`wifi_log` (
-  `wifi_log_id` INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS `who_there_db`.`wifi_log` (
+  `Wifi_log_id` INT NOT NULL AUTO_INCREMENT,
   `Room_Room_id` VARCHAR(45) NOT NULL,
-  `date` VARCHAR(45) NULL,
-  `time` VARCHAR(45) NULL,
-  `associated_client_counts` VARCHAR(45) NULL,
-  `authenticated_client_counts` VARCHAR(45) NULL,
-  PRIMARY KEY (`wifi_log_id`, `Room_Room_id`),
-  UNIQUE INDEX `wifi_log_id_UNIQUE` (`wifi_log_id` ASC),
+  `Date` DATE NULL,
+  `Time` TIME NULL,
+  `Associated_client_counts` INT NULL,
+  `Authenticated_client_counts` INT NULL,
+  PRIMARY KEY (`Wifi_log_id`),
+  UNIQUE INDEX `wifi_log_id_UNIQUE` (`Wifi_log_id` ASC),
   INDEX `fk_wifi_log_Room1_idx` (`Room_Room_id` ASC),
   CONSTRAINT `fk_wifi_log_Room1`
     FOREIGN KEY (`Room_Room_id`)
-    REFERENCES `mydb`.`Room` (`Room_id`)
+    REFERENCES `who_there_db`.`Room` (`Room_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`ground_truth_data`
+-- Table `who_there_db`.`ground_truth_data`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`ground_truth_data` (
-  `data_input_id` INT NOT NULL AUTO_INCREMENT,
-  `Room_Room_id` VARCHAR(45) NOT NULL,
-  `date` VARCHAR(45) NULL,
-  `time` VARCHAR(45) NULL,
+CREATE TABLE IF NOT EXISTS `who_there_db`.`ground_truth_data` (
+  `Data_input_id` INT NOT NULL AUTO_INCREMENT,
+  `Room_Room_id` VARCHAR(45) NULL,
+  `Date` DATE NULL,
+  `Time` TIME NULL,
   `Room_used` TINYINT(1) NULL,
-  `percentage_room_full` VARCHAR(45) NULL,
-  `no_of_people` VARCHAR(45) NULL,
+  `Percentage_room_full` FLOAT NULL,
+  `No_of_people` INT NULL,
   `lecture` TINYINT(1) NULL,
   `tutorial` TINYINT(1) NULL,
-  PRIMARY KEY (`data_input_id`, `Room_Room_id`),
+  PRIMARY KEY (`Data_input_id`),
   INDEX `fk_ground_truth_data_Room1_idx` (`Room_Room_id` ASC),
+  UNIQUE INDEX `Data_input_id_UNIQUE` (`Data_input_id` ASC),
   CONSTRAINT `fk_ground_truth_data_Room1`
     FOREIGN KEY (`Room_Room_id`)
-    REFERENCES `mydb`.`Room` (`Room_id`)
+    REFERENCES `who_there_db`.`Room` (`Room_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`users`
+-- Table `who_there_db`.`users`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`users` (
+CREATE TABLE IF NOT EXISTS `who_there_db`.`users` (
   `users_id` INT NOT NULL,
   `user_name` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NOT NULL,
@@ -129,9 +132,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`data_modeling_information`
+-- Table `who_there_db`.`data_modeling_information`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`data_modeling_information` (
+CREATE TABLE IF NOT EXISTS `who_there_db`.`data_modeling_information` (
   `data_input_id` INT NOT NULL,
   `data` FLOAT NULL,
   PRIMARY KEY (`data_input_id`))
