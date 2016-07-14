d584f08d1b938286cce60352e383360b92f932cc
cokelly <okellyconor@gmail.com>
2016-07-14 18:28:28 +0100

    Files will unzip in directory. Script checks that directory name ends with /

--- data_cleaning/data_input_manager.py
+++ data_cleaning/data_input_manager.py
@@ -10,6 +10,7 @@
 # be in their respective file's names
 
 import os
+import zipfile
 import pymysql
 import calendar
 # module imports
@@ -29,6 +30,13 @@ def phrase_data_and_input_into_database(db_host_name, db_user_name, db_password,
     # Convert db info into tuple
     db_tuple = (db_host_name, db_user_name, db_password, database_name, db_port)
 
+    # Check directory path ends with "/". If not add it.
+    if new_data_directory.endswith("/") == False:
+        new_data_directory += "/"
+
+    # Unzip files and remove zipped version
+    unzip_files_and_remove_zip(new_data_directory)
+
     # Get list of files in new data and remove those that are hidden
     new_files_list = os.listdir(new_data_directory)
     new_files_list = [file for file in new_files_list if file[0] is not "."]
@@ -38,6 +46,18 @@ def phrase_data_and_input_into_database(db_host_name, db_user_name, db_password,
         process_files(new_data_directory, new_files_list, db_tuple)
     else:
         return {"success": True, "new_data_exists": False, "data_input": False, "individual_file_reports": []}
+
+
+def unzip_files_and_remove_zip(directory):
+
+    # Iterated through directory. Find zip files. Unzip and remove ziped version
+    for item in os.listdir(directory):
+        if item.endswith(".zip"):
+            print(item)
+            zip_ref = zipfile.ZipFile(directory+item)
+            zip_ref.extractall(directory)
+            zip_ref.close()
+            os.remove(directory+item)
 
 
 def process_files(data_directory, file_list, db_tuple):
@@ -306,5 +326,5 @@ def input_file_into_db(data_to_be_input_tuple, db_host_name, db_user_name, db_pa
 
 if __name__ == '__main__':
     warnings.filterwarnings("ignore")
-    phrase_data_and_input_into_database("localhost", "root", "", "who_there_db")
+    phrase_data_and_input_into_database("localhost", "root", "", "who_there_db", new_data_directory="1.test_data")
     # input_file_into_db((0,0,0,0), "localhost", "root", "", "who_there_db",3306)
