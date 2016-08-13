59bb40c5c9aff84967ff72ce8816480173b2e3d4
DevCarl <Devin.stacey@ucdconnect.ie>
2016-08-13 19:53:49 +0100

    Provided comments for McvConfig.java

--- server/Csi42001Vm1ServerFiles/src/main/java/ie/ucd/serverjavafiles/MvcConfig.java
+++ server/Csi42001Vm1ServerFiles/src/main/java/ie/ucd/serverjavafiles/MvcConfig.java
@@ -12,27 +12,33 @@ import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 @Configuration
 public class MvcConfig extends WebMvcConfigurerAdapter {
     
+    // This method sets up a registry for login. This is not included in GreetingController due to the additional settings provided by SpringSecurity.
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/login").setViewName("login");
         registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
     }
     
+    // This method configures Autowired annotations set to the value dataSource. Any class using a dataSource should use @Autowired DataSource dataSource to connect to set up the datasource to these settings.
     @Bean(name = "dataSource")
     public DriverManagerDataSource dataSource() {
         DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
         driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
         driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/who_there_db?autoReconnect=true&useSSL=false");
+        // Our Project's MySql Database username is set to "root" and the password is set to "goldilocks"
         driverManagerDataSource.setUsername("root");
         driverManagerDataSource.setPassword("goldilocks");
         return driverManagerDataSource;
     }
-
+    
+    // This method configures the SpringSecurityDialect in order to allow Thymeleaf in HTML documents. No further config is necessary to use Thymeleaf
     @Bean
     public SpringSecurityDialect springSecurityDialect(){
         return new SpringSecurityDialect();
     }
     
+    // While HTML files do not require a dedicated resolver, I have included it in case we wish to have a template for resolving other resources, such as .JSP. 
+    // If resolving other types, you can set up the resolver similar to below to allow it to be resolved on our server via URL.
     @Bean
     public InternalResourceViewResolver viewResolver() {
 	InternalResourceViewResolver resolver = new InternalResourceViewResolver();
